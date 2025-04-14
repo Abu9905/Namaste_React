ec48e1e7378434409992c2eb37c86ef1d790a3b2
Abu shama <mabu53140@gmail.com>
2025-04-14 22:18:55 +0530

    EP-01

---
+++ App.js
@@ -0,0 +1,41 @@
+/* 
+<div id="parent">
+        <div id="child1">
+            <h1>Heading-1</h1>
+            <h2>Heading-2</h2>
+        </div>
+        <div id="child2">
+            <h1>Heading-1</h1>
+            <h2>Heading-2</h2>
+        </div>
+</div> 
+*/
+
+// ReactElement(Object) => HTML(Browser Understands)
+
+const parent = React.createElement("div", {id:"parent"},
+    [
+        React.createElement("div", {id:"child1"},
+            React.createElement("h1", {}, "Heading-1"),
+            React.createElement("h2", {}, "Heading-2")
+        ),
+        React.createElement("div", {id:"child2"},
+            React.createElement("h1", {}, "Heading-1"),
+            React.createElement("h2", {}, "Heading-2")
+        )
+    ]
+);
+
+// JSX:-
+
+
+// const heading = React.createElement(
+//     "h1", 
+//     {id:"heading",xyz:"abc"}, 
+//     "hello world from react"
+// );
+
+// console.log(heading); // object
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
