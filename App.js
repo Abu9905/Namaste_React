62340f25e61c8e1fca7d43312bee05c755049144
Abu shama <mabu53140@gmail.com>
2025-04-16 10:15:34 +0530

    EP-02

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 /* 
 <div id="parent">
         <div id="child1">
@@ -16,7 +18,7 @@
 const parent = React.createElement("div", {id:"parent"},
     [
         React.createElement("div", {id:"child1"},
-            React.createElement("h1", {}, "Heading-1"),
+            React.createElement("h1", {}, "This is a main heading "),
             React.createElement("h2", {}, "Heading-2")
         ),
         React.createElement("div", {id:"child2"},
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React Course
+
+
+
+# Parcel
+- dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File  Watching Alogorithm - C++ , write about it
+- Caching - Faster Builds
+- Image Optimization
+- Bundling 
+- Compressing 
+- Consistent Hashing
+- Code Splitting
+- Differential Building 
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree Shaking Alogorithm
